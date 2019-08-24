ad7138c3bbc6e0f0c4a5ccada1e83a73a2a9c584
BandiDuhHero <elibandimail@gmail.com>
2019-08-23 20:43:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let io = require('socket.io');
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
+let express = require('express');
+
+let app = express();
+
+let serv = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
 });
- 
-fs = require('fs'),
 
-index = fs.readFileSync(__dirname + '/index.html');()
+app.use('/client', express.static(__dirname + 'client', express.static(dirname + '/client')));
 
-var io = require('socket.io').listen(app);
+serv.listen(2000);
+
+let io = require('socket.io');
+
+io.sockets.on('connection', (socket) => {
+    console.log('Socket Connection');
+});
