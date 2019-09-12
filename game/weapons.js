e8b970bbbb9a311753d1ffab0abaf7829b749ae9
BandiDuhHero <elibandimail@gmail.com>
2019-09-12 10:15:10 -0400

    Update weapons.js

--- game/weapons.js
+++ game/weapons.js
@@ -1,1 +1,10 @@
+class Weapon {
+    constructor(damage, speed, range, cooldown) {
+        this.damage = damage;
+        this.speed = speed;
+        this.range = range;
+        this.cooldown = cooldown
+        this.health = health;
+    }
+}
 
