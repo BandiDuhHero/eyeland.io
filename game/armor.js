2bd7dcbf5d7398d2dd465d53e029bbc10fda8160
BandiDuhHero <elibandimail@gmail.com>
2019-09-15 15:17:28 -0400

    Update armor.js

--- game/armor.js
+++ game/armor.js
@@ -1,6 +1,11 @@
 class Armor {
-    constructor(durability, health) {
-        this.durability = durability;
-        this.health = health;
+    constructor(def, hp, regenamt, regentime, spd, atkspd) {
+        this.def = def;
+        this.hp = hp;
+        this.regenamt = regenamt;
+        this.regentime = regentime;
+        this.spd = spd;
+        this.atkspd = atkspd;
+        
     }
 }
